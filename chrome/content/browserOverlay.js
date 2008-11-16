f3478d579d8dec507ae288c7b0b5691b89cfde1c
Joshua McKenty <jmckenty@gmail.com>
2008-11-16 15:17:08 -0800

    Bug fixed with not showing install/update page (DOH!) Also incremented version number.

--- chrome/content/browserOverlay.js
+++ chrome/content/browserOverlay.js
@@ -457,7 +457,7 @@ function init() {
 
   prefs.setCharPref("lastversion", version);
   if (pageURL && pageURL != "null") {
-    setTimeout(function(){window.openUILinkIn(pageURL, "tab")}, SHOW_DELAY);
+    setTimeout(function(){window.openUILinkIn(pageURL, "tab")}, STARTUP_SHOW_DELAY);
   }
 
   if (FF3) {
